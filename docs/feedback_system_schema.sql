bf4e5c1ff2bcc4eb4e534d9a15ec997e34dacb18
AidaLuuppala <aida.luuppala@gmail.com>
2019-07-19 12:40:34 +0300

    database schema added

---
+++ docs/feedback_system_schema.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE feedback_system_db WITH ENCODING 'UTF8' LC_COLLATE='fi_FI.UTF8' LC_CTYPE='fi_FI.UTF8' TEMPLATE=template0;
+
+CREATE TABLE "feedback" (
+	"feedback_id" serial NOT NULL,
+	"feedback_date" DATE NOT NULL,
+	"feedback_type" varchar(100) NOT NULL,
+	"feedback_giver_type" varchar(100) NOT NULL,
+	"feedback_topic" varchar(100) NOT NULL,
+	"feedback_text" varchar(4000) NOT NULL,
+	"feedback_status" varchar(100) NOT NULL,
+	"wants_contact" BOOLEAN NOT NULL,
+	"language" varchar(100) NOT NULL,
+	"contact_firstname" varchar(50),
+	"contact_lastname" varchar(50),
+	"contact_email" varchar(100),
+	"contact_mobile" varchar(15),
+	CONSTRAINT "feedback_pk" PRIMARY KEY ("feedback_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "feedback_user" (
+    	"id" serial NOT NULL,
+    	"username" varchar(50) NOT NULL,
+    	"password" varchar(50) NOT NULL,
+    	"role" varchar(50) NOT NULL,
+    	"user_firstname" varchar(50) NOT NULL,
+    	"user_lastname" varchar(50) NOT NULL,
+    	"department" varchar(100) NOT NULL,
+    	CONSTRAINT "user_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
