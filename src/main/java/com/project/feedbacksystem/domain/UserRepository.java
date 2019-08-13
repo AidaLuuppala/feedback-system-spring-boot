f87a97e2626d7822167507a0e610fed33e8757ac
AidaLuuppala <aida.luuppala@gmail.com>
2019-08-13 14:23:16 +0300

    cleanup

--- src/main/java/com/project/feedbacksystem/domain/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.feedbacksystem.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-	 
-    User findByUsername(String username);
-    
-}
